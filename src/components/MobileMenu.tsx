f131848080324978c3b01d8f6e351c9a267ec020
Mike Shobes <mikeshobes718@gmail.com>
2026-01-01 20:43:08 -0300

    style: update How it works icon in mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -63,10 +63,10 @@ export default function MobileMenu() {
                 href: '/how-it-works', 
                 label: 'How it works', 
                 icon: (
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1.555.832l3.197-2.132a1 1 0 000-1.664z M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.989-2.39l-.548-.547z" />
                 ),
-                color: 'text-blue-600',
-                bg: 'bg-blue-50'
+                color: 'text-amber-600',
+                bg: 'bg-amber-50'
               },
               { 
                 href: '/features', 
